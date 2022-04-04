d6e8f30e65032e063f3f25f2567dbd30c8545a48
Vinkal-DAI <vinkal.meggi@dai.co.uk>
2022-04-04 17:20:57 +0100

    More commite 0- main - commit 1

--- Disco.sql
+++ Disco.sql
@@ -1,8 +1,10 @@
 Select * from HEATHROW_LONDON_AIRPORT;
 --adding COMMENT
 --Added comment in main branch 
+    -- Adding more comment into Main (commit 1 - main)
 --AIRPORT LHR
 Select * from HEATHROW_LONDON_AIRPORT
 where MAX_TEMP < 10;
 and MIN_TEMP >5
-and Rain < 50;
+and Rain < 50
+and 1 = 1 --Commit 1 - main;
