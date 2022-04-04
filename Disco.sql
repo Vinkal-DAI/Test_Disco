bbaf1991828989e7c9a730acfbfa0ee522400cec
Vinkal-DAI <vinkal.meggi@dai.co.uk>
2022-04-04 17:11:36 +0100

    Comment and updates in MAIN

--- Disco.sql
+++ Disco.sql
@@ -1,5 +1,8 @@
 Select * from HEATHROW_LONDON_AIRPORT;
 --adding COMMENT
+--Added comment in main branch 
+--AIRPORT LHR
 Select * from HEATHROW_LONDON_AIRPORT
-where MAX_TEMP <10;
-and MIN_TEMP >5;
+where MAX_TEMP < 10;
+and MIN_TEMP >5
+and Rain < 50;
