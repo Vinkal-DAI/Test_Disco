720ce27f4e95a79616fc33caeace2aac6334bfe4
Vinkal-DAI <vinkal.meggi@dai.co.uk>
2022-04-04 17:22:51 +0100

    add comment to 50 rain (commit 1 - Branch vs)

--- Disco.sql
+++ Disco.sql
@@ -6,5 +6,6 @@ Select * from HEATHROW_LONDON_AIRPORT
 where MAX_TEMP < 10
 and MIN_TEMP =0
 -- rain mor ethe 16 then use runaway 27L
-and rain > 16;
+and rain > 16
+--Rain about 50 is no concern (commit 1 - branch vs)
 and Rain < 50;
