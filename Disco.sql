3f5c5a27344f3b3f56f629d64cd44822c171969d
Vinkal-DAI <102296735+Vinkal-DAI@users.noreply.github.com>
2022-04-04 17:50:49 +0100

    Vs brnach 2.0 (#13)
    * Code for cold days (commit 1)
    
    * Commit 2 added frost days
    
    * added comments  commit 3
    
    * Commit 1
    
    * Commit 2

--- Disco.sql
+++ Disco.sql
@@ -13,4 +13,14 @@ and rain > 16
 -- need to include 49 (commit 2)
 and Rain < 49;
 -- THis the end of section 1 - Main commit after branching off of vs branch 2
-
+----------------------------------
+-- Cold days
+    -- Winter restritions
+Select * 
+FORM HEATHROW_LONDON_AIRPORT
+where MAX_TEMP < 5
+-- days when it freezes over night
+and AIR_FROST_DAYS <25;
+--add 1 - commit 1
+-- add 2 - commit 1
+-- add 3 - commit 2
