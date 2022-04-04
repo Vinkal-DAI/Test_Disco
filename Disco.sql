cce6f03979a1cba4bafaef2e44c6863ba63af567
Vinkal-DAI <vinkal.meggi@dai.co.uk>
2022-04-04 17:07:41 +0100

    Brnach vs added rain filter

--- Disco.sql
+++ Disco.sql
@@ -1,5 +1,6 @@
 Select * from HEATHROW_LONDON_AIRPORT;
 --adding COMMENT
 Select * from HEATHROW_LONDON_AIRPORT
-where MAX_TEMP <10;
-and MIN_TEMP >5;
+where MAX_TEMP <10
+and MIN_TEMP =0
+and rain > 16;
