32d737e8aed844c969123bea636d6b230739c42f
Vinkal-DAI <102296735+Vinkal-DAI@users.noreply.github.com>
2022-04-04 17:41:01 +0100

    changes after branching of vs branch 2

--- Disco.sql
+++ Disco.sql
@@ -12,4 +12,5 @@ and rain > 16
 --Rain about 50 is no concern (commit 1 - branch vs)
 -- need to include 49 (commit 2)
 and Rain < 49;
+-- THis the end of section 1 - Main commit after branching off of vs branch 2
 
